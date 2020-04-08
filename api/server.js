ae915380e5ac89753c2920be64419846d0cd92b0
raudelf <rawdell@outlook.com>
2020-04-08 18:51:08 -0500

    refactored to renamed filed

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
+// Imports
+const GetRouter = require('../posts/get-router.js');
+
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/posts', GetRouter);
 
 module.exports = server;
