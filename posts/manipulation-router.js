a2aef321ab0b3048a62cf5ba7d4d98bb043a2f11
raudelf <rawdell@outlook.com>
2020-04-08 19:50:59 -0500

    finished the delete endpoint and met MVP

--- posts/manipulation-router.js
+++ posts/manipulation-router.js
@@ -26,4 +26,20 @@ router.put('/:id', (req, res) => {
         })
 });
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db('accounts').where('id', id).del()
+        .then(account => {
+            if (account) {
+                res.status(200).json({ message: 'Account deleted'});
+            } else {
+                res.status(404).json({ error: `Account ${id} does not exist` });
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error deleting account' })
+        })
+});
+
 module.exports = router;
